fe042325c9bd7f753b41047c9f55bf7e5143150c
Pterygoidien <bertrand.bouillon@gmail.com>
2021-12-21 09:46:15 +0100

    Version 1.0.1 Bêta

--- transports/forms.py
+++ transports/forms.py
@@ -5,12 +5,12 @@ from django.contrib.admin import widgets
 
 class StationForm(forms.Form):
     departure = forms.ModelChoiceField(
-        queryset=Station.objects.all().order_by('Station_name'),
-        empty_label=None,
+        queryset=Station.objects.all(),
+        empty_label='Sélectionnez un arrêt',
         label='Arrêt de départ'
     )
     arrival = forms.ModelChoiceField(
-        queryset=Station.objects.all().order_by('Station_name'),
-        empty_label=None,
+        queryset=Station.objects.all(),
+        empty_label='Sélectionnez un arrêt',
         label='Arrêt d\'arrivée'
     )
